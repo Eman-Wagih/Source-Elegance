f5de521066ffd54929f6edc02a6394700a75d288
Eman Wagih <e.wagih95@gmail.com>
2026-01-05 13:47:39 +0200

    add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,20 @@ import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
 import SignupPage from "./pages/SignUp";
 import { ToastContainer } from "react-toastify";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/Login";
 function App() {
   return (
-    <>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <SignupPage />
-        <ToastContainer />
-      </ThemeProvider>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+
+      <ToastContainer />
+    </ThemeProvider>
   );
 }
 
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
+import { LoginForm } from "@/components/login-form";
 import { ModeToggle } from "@/components/mode-toggler";
-import { SignupForm } from "../components/signup-form";
-import shoppingImage from "/shopping-bags-set-credit-card-woman-s-hand.jpg";
-export default function SignupPage() {
+import shoppingImg from "/front-view-woman-holding-lots-shopping-bags.jpg";
+export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
@@ -10,13 +10,13 @@ export default function SignupPage() {
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
-            <SignupForm />
+            <LoginForm />
           </div>
         </div>
       </div>
-      <div className="bg-muted relative hidden lg:block min-h-full">
+      <div className="bg-muted relative hidden lg:block">
         <img
-          src={shoppingImage}
+          src={shoppingImg}
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover"
         />
